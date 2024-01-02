a8d9ab0cf8bdf124dfffcf72a51b38bb400aae32
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2024-01-02 09:37:23 +0000

    Initial commit

---
+++ src/main/java/coc/recruiter/CocRecruiterApplication.java
@@ -0,0 +1,13 @@
+package coc.recruiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocRecruiterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CocRecruiterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/coc/recruiter/CocRecruiterApplicationTests.java
@@ -0,0 +1,13 @@
+package coc.recruiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocRecruiterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
