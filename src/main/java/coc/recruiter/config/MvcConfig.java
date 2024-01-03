f4e7273c4b42c6be834b681a42b6549f598b3a68
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2024-01-03 16:24:51 +0000

    Adds simple Frontend

--- src/main/java/coc/recruiter/config/MvcConfig.java
+++ src/main/java/coc/recruiter/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package coc.recruiter.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,14 @@ public class MvcConfig implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/**")
         .addResourceLocations("classpath:/static/");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*") // Allow requests from any origin
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("*")
+        .allowCredentials(true);
   }
 }
