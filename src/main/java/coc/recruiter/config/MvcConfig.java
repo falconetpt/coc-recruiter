db47853dde4701d7e28f15ab98719550c4af139e
Ricardo Gomes <ricardo.gomes@currencycloud.com>
2024-01-03 16:29:57 +0000

    Adds simple Frontend

--- src/main/java/coc/recruiter/config/MvcConfig.java
+++ src/main/java/coc/recruiter/config/MvcConfig.java
@@ -17,7 +17,7 @@ public class MvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOriginPatterns("*")  // Use allowedOriginPatterns instead of allowedOrigins
+        .allowedOriginPatterns("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .allowCredentials(true);
